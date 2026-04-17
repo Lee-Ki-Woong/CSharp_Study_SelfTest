0a5cfaaf68eabd338b809133d1fc710b1d382e1c
LeeKiWoong <littlemanner1@gmail.com>
2026-04-17 20:44:06 +0900

    [클래스와 필드, 클래스의 생성 및 값 할당]CSharp_Study_SelfTest_002
    기초적인 클래스 정의 및 필드(맴버 변수)를 선언, Main에서 객체의 생성 및 필드에 값 할당

--- CSharp_Study_SelfTest/Program.cs
+++ CSharp_Study_SelfTest/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSharp_Study_SelfTest
 {
+    internal class MyClass
+    {
+        public string m_name;
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            MyClass myClass = new MyClass();
+            myClass.m_name = "가";
 
+            Console.WriteLine(myClass.m_name);
         }
     }
 }
