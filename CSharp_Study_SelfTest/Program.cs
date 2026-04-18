6a9888ab212afca3a4e799b4e05a04567e2193f8
LeeKiWoong <littlemanner1@gmail.com>
2026-04-18 11:24:08 +0900

    [클래스 - 캡슐화 및 접근 제한자]CSharp_Study_SelfTest_005
    접근 제한자 private로 오직 자기 자신만 접근 가능하게끔 만들기!
    접근 제한자 protected로 자기 자신과 자신을 상속한 자식만 접근 가능하게끔 만들기!

--- CSharp_Study_SelfTest/Program.cs
+++ CSharp_Study_SelfTest/Program.cs
@@ -9,28 +9,34 @@ namespace CSharp_Study_SelfTest
 {
     internal class Entity
     {
-        public string m_id;
+        private string m_id;
         public string m_name;
+        protected int m_attack;
         
-        public Entity(string id, string name)
+        public Entity(string id, string name, int attack)
         {
             m_id = id;
             m_name = name;
+            m_attack = attack;
         }
 
         public void Shout()
         {
             Console.WriteLine("크아앙!!");
         }
-
-    
     }
     internal class Player : Entity
     {
-        public Player(string id, string name) : base(id, name)
+        public Player(string id, string name, int attack) : base(id, name, attack)
         {
 
         }
+
+        public void MyAttack()
+        {
+            Console.WriteLine($"내 공격력은 {m_attack}이다!!");
+        }
+        
     }
     
 
@@ -38,9 +44,19 @@ namespace CSharp_Study_SelfTest
     {
         static void Main(string[] args)
         {
-            Player player = new Player("1", "기웅");
+            Player player = new Player("1", "기웅", 5);
 
+            Entity entity = new Entity("2", "슬라임", 1);
+
+            //접근 제한자 public은 어디서든 수정 및 접근이 가능하다!!
+            entity.m_name = "바다 슬라임";
             player.Shout();
+
+            //접근 제한자 private는 오직 자신만 수정 및 접근이 가능하다!!
+            //entity.m_id = "3";
+
+            //접근 제한자 protected는 부모와 자식만 접근이 가능하다!!
+            player.MyAttack();
         }
     }
 }
