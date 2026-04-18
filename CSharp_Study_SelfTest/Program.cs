36292482cbcaca1e1514948c1fe4891f8827d16e
LeeKiWoong <littlemanner1@gmail.com>
2026-04-18 11:33:39 +0900

    [클래스 - Getter 메서드 및 Setter 메서드]CSharp_Study_SelfTest_006
    Set함수는 접근 제한이 걸린 필드를 외부에서 수정할 수 있도록 만들 수 있다!
    Get함수는 접근 제한이 걸린 필드를 외부에서 볼 수 있도록 만들 수 있다!

--- CSharp_Study_SelfTest/Program.cs
+++ CSharp_Study_SelfTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -10,7 +11,7 @@ namespace CSharp_Study_SelfTest
     internal class Entity
     {
         private string m_id;
-        public string m_name;
+        protected string m_name;
         protected int m_attack;
         
         public Entity(string id, string name, int attack)
@@ -18,6 +19,16 @@ namespace CSharp_Study_SelfTest
             m_id = id;
             m_name = name;
             m_attack = attack;
+        }
+
+        public string GetName()
+        {
+            return m_name;
+        }
+
+        public void SetName(string name)
+        {
+            m_name = name;
         }
 
         public void Shout()
@@ -49,7 +60,6 @@ namespace CSharp_Study_SelfTest
             Entity entity = new Entity("2", "슬라임", 1);
 
             //접근 제한자 public은 어디서든 수정 및 접근이 가능하다!!
-            entity.m_name = "바다 슬라임";
             player.Shout();
 
             //접근 제한자 private는 오직 자신만 수정 및 접근이 가능하다!!
@@ -57,6 +67,16 @@ namespace CSharp_Study_SelfTest
 
             //접근 제한자 protected는 부모와 자식만 접근이 가능하다!!
             player.MyAttack();
+
+            //protected로 접근 제한이 걸린 필드를 Get함수 및 Set함수로 외부에서 수정 및 값 가져오기를 하기!!
+            
+            //Set메서드는 접근 제한이 걸린 필드를 외부에서 수정할 수 있게 만든다!!
+            entity.SetName("조그마한 슬라임");
+
+            //Get메서드는 접근 제한이 걸린 필드를 외부에서 볼 수 있도록 만든다!!
+            Console.WriteLine(entity.GetName());
+
+
         }
     }
 }
