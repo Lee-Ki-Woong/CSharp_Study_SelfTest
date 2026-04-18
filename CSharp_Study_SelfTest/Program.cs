4eaca3481fecdc658b3f4f6721ffd94f5236447e
LeeKiWoong <littlemanner1@gmail.com>
2026-04-18 13:27:04 +0900

    [메모리 관리 - 참조형의 할당과 GC의 구동 방식]CSharp_Study_SelfTest_007
    참조형 변수는 스택 메모리에 실제 데이터의 주소(포인터)를, 힙에 실제 데이터(인스턴스)를 할당한다!
    참조가 끊기면(=스택 메모리에서 변수가 사라지면) 데이터는 가비지가 되어 가비지 컬렉터의 대상이 된다!!
    가비지 컬렉터는 즉시 가동되는 게 아니라, 스케쥴에 따라서 가비지를 청소(=메모리 해제)한다!!

--- CSharp_Study_SelfTest/Program.cs
+++ CSharp_Study_SelfTest/Program.cs
@@ -8,8 +8,21 @@ namespace CSharp_Study_SelfTest
 {
     internal class Program
     {
+        public static void Text()
+        {
+            // 2) textOne 변수가 스택 메모리에 생성이 되며
+            // 3) textOne이 가르키는(포인터) 문자열 데이터(인스턴스)가 힙 메모리에 할당이 된다!!
+            string textOne = "문자열 데이터는 참조형! 참조형이니 생성 시 힙 메모리에 저장되며, 참조가 끊길 경우 GC에 의해 정리가 될 수 있다!!";
+            Console.WriteLine(textOne);
+
+        }
+
+
         static void Main(string[] args)
         {
+            // 1) Text 메서드로 들어가면,
+            Text();
+            // 3) Text 메서드를 나오게 되면 textOne이 스택 메모리에서 사라져 참조가 끊겨, 힙 메모리에 할당 된 "" GC의 대상이 되어 나중에 GC가 구동되면 메모리 헤제가 된다!!
 
         }
     }
